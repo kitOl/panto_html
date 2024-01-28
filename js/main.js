7410ee55d9c30d5164bfbd4dfa2d704084eb5a2b
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-28 08:30:37 +0300

    add active elements to header

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const infoBtns = document.querySelectorAll(".info-dot");
+const infoHints = document.querySelectorAll(".info-hint");
+
+for (let btn of infoBtns) {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    this.parentNode.querySelector(".info-hint").classList.remove("none");
+  });
+}
+
+document.addEventListener("click", function () {
+  for (let hint of infoHints) {
+    hint.classList.add("none");
+  }
+
+  for (let hint of infoHints) {
+    hint.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+  }
+});
